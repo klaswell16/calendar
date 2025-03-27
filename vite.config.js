eb38114461ae798cee1c2a72ee95a9e723b04d18
Kayan Laswell <kcclaswell@gmail.com>
2025-03-27 18:39:20 -0400

    add base to config

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,10 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-module.exports = {
-  publicPath: '/calendar/',
-}
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/calendar/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
