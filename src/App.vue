0800668197808078ba0c76fd10051c45263bec22
Kayan Laswell <kcclaswell@gmail.com>
2025-03-27 15:19:42 -0400

    add zoom-container to increase the size of everything

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
 export default {
   data() {
     return {
+      month: '',
+      day: '',
       hours: Array.from({ length: 24 }, (_, i) => i),
       selectedHour: null,
       isActive: false,
@@ -18,8 +20,10 @@ export default {
       this.isActive = !this.isActive
     },
     addPerson() {
-      this.people.push(this.newPerson)
-      this.newPerson = ''
+      if (this.newPerson.trim()) {
+        this.people.push(this.newPerson.trim())
+        this.newPerson = ''
+      }
     },
     deletePerson(index) {
       this.people.splice(index, 1)
@@ -45,53 +49,69 @@ export default {
 </script>
 
 <template>
-  <header>
-    Date:
-    <input v-model="month" type="text" style="width: 50px" class="monthText" placeholder="M" />
-    /
-    <input v-model="day" type="text" style="width: 50px" class="dayText" placeholder="D" />
-  </header>
-  <hr />
-  <div>
-    <h2>
-      Event:
-      <input v-model="event" type="text" class="eventText" placeholder="Enter Name of Event" />
-    </h2>
-  </div>
-
-  <hr />
-  <h3>
-    Add People
-    <button @click="toggle" :class="{ active: isActive }">+</button>
-  </h3>
-  <ul>
-    <li v-for="(person, index) in people" :key="index">
-      {{ person }}
-      <button @click="deletePerson(index)">-</button>
-    </li>
-  </ul>
-  <main>
-    <div v-show="isActive">
-      <input v-model="newPerson" type="text" placeholder="Enter Person" />
-      <button @click="addPerson">Add Person</button>
-
-      <h4>People recently Added:</h4>
-
-      <div v-show="showSam">Sam <button @click="addSam">+</button></div>
-      <div v-show="showJenny">Jenny <button @click="addJenny">+</button></div>
-      <div v-show="showGavin">Gavin <button @click="addGavin">+</button></div>
-    </div>
+  <div class="zoom-container">
+    <header>
+      Date:
+      <input v-model="month" type="text" style="width: 50px" class="monthText" placeholder="M" />
+      /
+      <input v-model="day" type="text" style="width: 50px" class="dayText" placeholder="D" />
+    </header>
+    <hr />
     <div>
-      <label for="hour-select">Time:</label>
-      <select id="hour-select" v-model="selectedHour">
-        <option v-for="hour in hours" :key="hour" :value="hour">{{ formatHour(hour) }}</option>
-      </select>
-      <p>Hour: {{ formatHour(selectedHour) }}</p>
+      <h2>
+        Event:
+        <input v-model="event" type="text" class="eventText" placeholder="Enter Name of Event" />
+      </h2>
     </div>
-  </main>
+
+    <hr />
+    <h3>
+      Add People
+      <button @click="toggle" :class="{ active: isActive }">+</button>
+    </h3>
+    <ul>
+      <li v-for="(person, index) in people" :key="index">
+        {{ person }}
+        <button @click="deletePerson(index)">-</button>
+      </li>
+    </ul>
+    <main>
+      <div v-show="isActive">
+        <input
+          v-model="newPerson"
+          type="text"
+          placeholder="Enter Person"
+          @keyup.enter="addPerson"
+        />
+        <button @click="addPerson">Add Person</button>
+
+        <h4>People recently Added:</h4>
+        <div v-show="showSam">Sam <button @click="addSam">+</button></div>
+        <div v-show="showJenny">Jenny <button @click="addJenny">+</button></div>
+        <div v-show="showGavin">Gavin <button @click="addGavin">+</button></div>
+      </div>
+      <hr />
+      <div>
+        <label for="hour-select">Time:</label>
+        <select id="hour-select" v-model="selectedHour">
+          <option v-for="hour in hours" :key="hour" :value="hour">
+            {{ formatHour(hour) }}
+          </option>
+        </select>
+        <p>Hour: {{ formatHour(selectedHour) }}</p>
+      </div>
+    </main>
+  </div>
 </template>
 
 <style scoped>
+.zoom-container {
+  transform: scale(1.5);
+  transform-origin: 0 0;
+  width: 66.67%;
+  padding: 20px;
+}
+
 header {
   font-size: 300%;
 }
@@ -99,36 +119,25 @@ header {
   background-color: transparent;
   border: none;
   outline: none;
+  color: whitesmoke;
+  font-size: 22px;
 }
 .eventText:focus {
   box-shadow: 0 0 2px rgba(0, 0, 0, 0.3);
 }
-.eventText[type='text'] {
-  color: whitesmoke;
-  font-size: 22px;
-}
-.monthText {
-  background-color: transparent;
-  border: none;
-  outline: none;
-}
-.monthText:focus {
-  box-shadow: 0 0 2px rgba(0, 0, 0, 0.3);
-}
-.monthText[type='text'] {
-  color: whitesmoke;
-  font-size: 40px;
-}
+.monthText,
 .dayText {
   background-color: transparent;
   border: none;
   outline: none;
+  color: whitesmoke;
+  font-size: 40px;
 }
+.monthText:focus,
 .dayText:focus {
   box-shadow: 0 0 2px rgba(0, 0, 0, 0.3);
 }
-.dayText[type='text'] {
-  color: whitesmoke;
-  font-size: 40px;
+button {
+  cursor: pointer;
 }
 </style>
